de74862023c70de5f2448630e227470d37340162
MaiMacWebHes <mweberhess01@saintmarys.edu>
2024-08-28 17:25:45 -0400

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,22 @@
+# the authors' name are: Maggie and Mac
+print("a\nb\nc")
+print("Hello"+"World!")
+print("Hello\n" + "\nWorld!")
+print("Hello\n\nWorld!")
+# Adapt:
+print("123\n45\n6")
+# C: Aritmetic Operations
+print(1+2*3)
+print((1+2)*3)
+print(7/3)
+print(7//3)
+print(7%3)
+print(22/4)
+print(22//4)
+print(22%4)
+print(3**2)
+print(2**3)
+#C: Adapt
+print(1+2*3**4)
+print("2+2=", 2+2)
+print("50 pounds is ", 50*0.453592, "kilograms") 
